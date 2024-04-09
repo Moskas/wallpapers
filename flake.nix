900c77807c86a865421f133c7a0e91c9d2620cb4
Moskas <minemoskas@gmail.com>
2024-04-09 15:17:25 +0200

    Added nix flake

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "Wallpapers flake";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        version = "09.04.2024";
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell { buildInputs = [ ]; };
+        packages = {
+          default = stdenv.mkDerivation {
+            name = "wallpapers";
+            inherit version;
+            src = ./.;
+            buildPhase = ''
+              mkdir -p $out
+            '';
+            installPhase = ''
+              cp -rv $src/* $out
+            '';
+          };
+          gruvbox = stdenv.mkDerivation {
+            name = "gruvbox";
+            inherit version;
+            src = ./solarized;
+            buildPhase = ''
+              mkdir -p $out
+            '';
+            installPhase = ''
+              cp -rv $src/* $out
+            '';
+          };
+          solarized = stdenv.mkDerivation {
+            name = "solarized";
+            inherit version;
+            src = ./solarized;
+            buildPhase = ''
+              mkdir -p $out
+            '';
+            installPhase = ''
+              cp -rv $src/* $out
+            '';
+          };
+        };
+      }
+    );
+}
